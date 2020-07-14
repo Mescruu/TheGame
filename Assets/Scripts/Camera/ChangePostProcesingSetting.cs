643bac4b20d1a6d5e84aa56a5e2a3ba346d9f2e6
Mescruu <mescruu@gmail.com>
2020-07-14 17:53:59 +0200

    Changing the post processing effect

--- Assets/Scripts/Camera/ChangePostProcesingSetting.cs
+++ Assets/Scripts/Camera/ChangePostProcesingSetting.cs
@@ -4,20 +4,25 @@ using UnityEngine;
 
 public class ChangePostProcesingSetting : MonoBehaviour {
 
+    //Ustawienia postprocesingu (profil)
     public CameraProfile camProfile;
-    private float chAberration;
-    private float bloom;
 
+    private float chAberration; //wartość aberacji
+    private float bloom; //wartość efeketu bloom
+
+    //zmienne boolean odpowiadajace określające aktualną zmianę
     public bool ChangeCamSet;
     public bool ChangeAberration;
     public bool ChangeMusic;
     public bool ChangeBloom;
+    private bool StartChange;
+
+    //Wartości intensywności i czasu trwania zmiany
     public float BloomSpeed=4f;
     public float bloomBorder=10f;
     public float Timer;
     public float TimerCD;
-    private bool StartChange;
-    // Use this for initialization
+
    void Start()
     {
         StartChange = false;
@@ -26,6 +31,7 @@ public class ChangePostProcesingSetting : MonoBehaviour {
     // Update is called once per frame
     void Update () {
 
+        //Przygotowanie do zmian ustawień kamery, pobranie odpowiednich danych
         if (ChangeCamSet)
         {
             Timer = TimerCD;
@@ -34,72 +40,56 @@ public class ChangePostProcesingSetting : MonoBehaviour {
 
             if (!camProfile.audioSource.isPlaying)
             {
-                camProfile.audioSource.clip = camProfile.ChAberrationaudioClip;
+                camProfile.audioSource.clip = camProfile.ChAberrationaudioClip; //włączenie dźwięku odpowiadającego za zmianę efektu
                 camProfile.audioSource.Play();
             }
             StartChange = true;
             ChangeCamSet = false;
-
         }
+
+        //Rozpoczęcie zmian
         if (StartChange)
         {
-            Timer -= Time.deltaTime;
-
-          
+            Timer -= Time.deltaTime; 
+            
+            //Powrót do poprzednich ustawień
                 if (Timer <= 0)
                 {
                     if (ChangeAberration)
-                    {
                         camProfile.ChangeChAberrationAtRuntimeToPreviousSetting();
-                    }
+
                     if (ChangeBloom)
-                    {
                         camProfile.ChangeBloomAtRuntimeToPreviousSetting();
-                    }
+
                 StartChange = false;    
                 }
                 else
                 {
-                     if (ChangeAberration)
-                     {
+                    if (ChangeAberration)
                         chAberration += Time.deltaTime / 3;
-                    }
-                if (ChangeBloom)
-                {
-                    bloom += Time.deltaTime*BloomSpeed;
-                }
-                if (ChangeMusic)
-                       {
-                       camProfile.audioSource.volume = chAberration;
-                     }
+                     
+                     if (ChangeBloom)
+                        bloom += Time.deltaTime*BloomSpeed;
+                     
+                     if (ChangeMusic)
+                         camProfile.audioSource.volume = chAberration;
+                     
 
-                if (chAberration <= 1)
-                    {
+                     if (chAberration <= 1)
                         camProfile.ChangeChAberrationAtRuntime(chAberration, ChangeMusic);
-                    }
-                    else
-                    {
+                     else
                         camProfile.ChangeChAberrationAtRuntime(1,ChangeMusic);
-
-                    }
-                if (bloom <= bloomBorder)
-                {
-                    camProfile.ChangeBloomAtRuntime(bloom);
-                }
-                else
-                {
+                   
+                    if (bloom <= bloomBorder)
+                     camProfile.ChangeBloomAtRuntime(bloom);
+                    else
                     camProfile.ChangeBloomAtRuntime(bloomBorder);
                 }
-            }
-
-            }
-            else
-            {
-                chAberration = 0;
-            }
         }
-       
-
-
-    }
+        else
+        {
+        chAberration = 0;
+        }
+     }
+}
 
