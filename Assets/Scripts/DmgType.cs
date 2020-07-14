a4c65d15712638f2d212fd5a5c7d41f15eab90e3
Mescruu <mescruu@gmail.com>
2020-07-14 16:56:41 +0200

    Types of damage

--- Assets/Scripts/DmgType.cs
+++ Assets/Scripts/DmgType.cs
@@ -1,14 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-/**
- * Enum zawierający typy obiektów mogących wystąpić w ekwipunku.
- * 
- * @author Hubert Paluch.
- * MViRe - na potrzeby kursu UNITY3D v5.
- * mvire.com 
- */
+ //Enum zawierający typy obrażeń
+
 public enum DmgType
 {
-    Meele, Fire, Elect, Chaos, Ankh, Poison,None
+    Meele, Fire, Elect, Chaos, Ankh, Poison,None 
 }
