1c7c0ea7bd1ecf0a761bcb356215b6811694e02d
Mescruu <mescruu@gmail.com>
2020-07-16 21:13:42 +0200

    Falling platform

--- Assets/Scripts/Envoirment/Falling_Platform.cs
+++ Assets/Scripts/Envoirment/Falling_Platform.cs
@@ -3,27 +3,25 @@ using System.Collections;
 
 public class Falling_Platform : MonoBehaviour {
 
+
 	private Rigidbody2D rb2d;
-
-	public float destroyTime;
-
-	public float fallDelay;
+	public float destroyTime; //czas po którym obiekt zniknie
+	public float fallDelay; //czas po jakim platforma opadnie
 
 	void Start()
 	{
 		rb2d = GetComponent<Rigidbody2D> ();
 	}
 
-
 	void OnCollisionEnter2D(Collision2D col)
 	{
 		if (col.collider.CompareTag ("Player")) 
-				{
-				StartCoroutine(Fall());
-				}
+		{
+		StartCoroutine(Fall());
+		}
 	}
 
-	IEnumerator Fall()
+	IEnumerator Fall() //sekwencja wykonywane klatka po klatce gry
 	{
 		yield return new WaitForSeconds (fallDelay);
 		rb2d.isKinematic = false;
