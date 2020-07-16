ebf6d440e4bd123768e80a6d99867f9f15e47d64
Mescruu <mescruu@gmail.com>
2020-07-16 17:14:22 +0200

    Parabolic throw mechanic

--- Assets/Scripts/Enemy/ParableThrowing.cs
+++ Assets/Scripts/Enemy/ParableThrowing.cs
@@ -4,24 +4,31 @@ using System.Collections.Generic;
 
 public class ParableThrowing : MonoBehaviour
 {
+    //typ obrażeń
     [Tooltip("Type of dmg")]
     public DmgType[] dmgType;
 
+    //Ilość obrażeń
     [Tooltip("Count of dmg")]
     public float dmg;
+    
+    //Procentowe rozłożenie obrażeń w przypadku obrażeń o wielu typach
     [Tooltip("Percentage Distribution of all dmg's types for example: 0.1 = 10% ")]
-    public float[] percentageDistribution;  //rozklad obrazen
+    public float[] percentageDistribution;
 
-
+    //Cel
     [Tooltip("Position we want to hit")]
     public Vector3 targetPos;
 
+    //Predkość pozioma
     [Tooltip("Horizontal speed, in units/sec")]
     public float speed = 10;
 
+    //Prędkość pionowa
     [Tooltip("How high the arc should be, in units")]
     public float arcHeight = 1;
 
+    
     Vector3 startPos;
     private Player_Controller player;
     private Destroy_obj destr;
@@ -30,27 +37,22 @@ public class ParableThrowing : MonoBehaviour
     public float middleRange;
     public float farRange;
 
-
-
-
     void Start()
     {
+        //Przypisanie komponentów i celu
         targetPos = new Vector3(GameObject.FindGameObjectWithTag("Player").transform.position.x, GameObject.FindGameObjectWithTag("Player").transform.position.y, GameObject.FindGameObjectWithTag("Player").transform.position.z);
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player_Controller>();
-
-        // Cache our start position, which is really the only thing we need
-        // (in addition to our current position, and the target).
-        startPos = transform.position;
         destr = gameObject.GetComponent<Destroy_obj>();
 
-        if(Mathf.Abs(player.transform.position.x)> farRange)
+        //Początkowe wartości rzutu obiektu po paraboli
+        startPos = transform.position;
+        if (Mathf.Abs(player.transform.position.x)> farRange)
         {
             arcHeight = Mathf.Abs(player.transform.position.x / 2f) * Mathf.Tan(Mathf.PI / 4f);
         }
         if (Mathf.Abs(player.transform.position.x) <= farRange && Mathf.Abs(player.transform.position.x) > middleRange)
         {
             arcHeight = Mathf.Abs(player.transform.position.x / 2f) * Mathf.Tan(Mathf.PI / 4f)/2f;
-
         }
         if (Mathf.Abs(player.transform.position.x)>=0&& Mathf.Abs(player.transform.position.x)<=middleRange)
         {
@@ -60,7 +62,7 @@ public class ParableThrowing : MonoBehaviour
 
     void Update()
     {
-        // Compute the next position, with arc added in
+        // Obliczanie następnej pozycji obiektu
         float x0 = startPos.x;
         float x1 = targetPos.x;
         float dist = x1 - x0;
@@ -72,7 +74,6 @@ public class ParableThrowing : MonoBehaviour
             Vector3 nextPos = new Vector3(nextX, baseY + arc, transform.position.z);
             transform.rotation = LookAt2D(nextPos - transform.position);
             transform.position = nextPos;
-
         }
         else
         {
@@ -83,21 +84,14 @@ public class ParableThrowing : MonoBehaviour
             transform.rotation = LookAt2D(nextPos - transform.position);
             transform.position = nextPos;
         }
-
-        // Rotate to face the next position, and then move there
-
-
-        // Do something when we reach the target
-   
-
     }
 
+    //W przypadku gdy obiekt dotrze
     void Arrived()
     {
         Destroy(gameObject);
     }
 
-    /// 
     /// This is a 2D version of Quaternion.LookAt; it returns a quaternion
     /// that makes the local +X axis point in the given forward direction.
     /// 
@@ -107,52 +101,39 @@ public class ParableThrowing : MonoBehaviour
     {
         return Quaternion.Euler(0, 0, Mathf.Atan2(forward.y, forward.x) * Mathf.Rad2Deg);
     }
+
+    //W przypadku gdy obiekt osiągnie gracza badz ziemi/sciany
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
             if (!player.dead)
             {
-
-
                 Vector3 point;
-
                 point = other.gameObject.GetComponent<Collider2D>().bounds.ClosestPoint(transform.position);
-
-              
-                    player.getDmg(dmg, dmgType, percentageDistribution, point, transform);
+                player.getDmg(dmg, dmgType, percentageDistribution, point, transform);
 
                 if (destr!=null)
                 {
                     destr.TimeToDestroy = 0;
                 }
-
             }
         }
         if (other.tag == "Shield")
         {
             if (!player.dead)
             {
-
-
                 Vector3 point;
-
                 point = other.gameObject.GetComponent<Collider2D>().bounds.ClosestPoint(transform.position);
-
-
                 other.GetComponent<Shield>().getDmg(dmg, dmgType, percentageDistribution, point, transform);
-
                 if (destr != null)
                 {
                     destr.TimeToDestroy = 0;
                 }
-
             }
         }
         if (other.tag == "Ground")
         {
-          
-
                 if (destr != null)
                 {
                     destr.TimeToDestroy = 0;
@@ -161,12 +142,9 @@ public class ParableThrowing : MonoBehaviour
                 {
                     Destroy(gameObject);
                 }
-
         }
         if (other.tag == "Wall")
         {
-
-
             if (destr != null)
             {
                 destr.TimeToDestroy = 0;
@@ -175,8 +153,6 @@ public class ParableThrowing : MonoBehaviour
             {
                 Destroy(gameObject);
             }
-
         }
     }
-
 }
