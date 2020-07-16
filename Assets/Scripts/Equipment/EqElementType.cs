c6211474e02892632a7266a827527a45356ef165
Mescruu <mescruu@gmail.com>
2020-07-16 21:43:56 +0200

    Types of eq elements

--- Assets/Scripts/Equipment/EqElementType.cs
+++ Assets/Scripts/Equipment/EqElementType.cs
@@ -1,13 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-/**
- * Enum zawierający typy obiektów mogących wystąpić w ekwipunku.
- * 
- * @author Hubert Paluch.
- * MViRe - na potrzeby kursu UNITY3D v5.
- * mvire.com 
- */
+//Enum zawierający typy obiektów mogących wystąpić w ekwipunku.
 public enum EqElementType
 {
 	Main_Weapon, Second_Weapon, Task_Item, Amulet, Armor, Legs,Boots,Rest,Defend,Weapons,Chest
