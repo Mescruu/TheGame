5380e627cc82ad898dbacfe8ecafd1d2587949bf
Mescruu <mescruu@gmail.com>
2020-07-16 21:00:27 +0200

    Receiving dmg (doesnt use)

--- Assets/Scripts/Enemy/takeDamage.cs
+++ Assets/Scripts/Enemy/takeDamage.cs
@@ -5,59 +5,36 @@ using UnityEngine;
 public class takeDamage : MonoBehaviour
 {
 
-    public DmgType[] dmgType;
-    public float dmg;
+    public DmgType[] dmgType; //Typ obrażeń
+    public float dmg; //ilość obrażeń
     public float[] percentageDistribution;  //rozklad obrazen
 
-
-
+    //Gracz
     private Player_Controller player;
 
-    // Use this for initialization
     void Start()
     {
-
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player_Controller>();
-
     }
-    void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerEnter2D(Collider2D other) //Otrzymywanie obrażeń 
     {
         if (other.tag == "Player")
         {
             if (!player.dead)
             {
-
-
                 Vector3 point;
-
                 point = other.gameObject.GetComponent<Collider2D>().bounds.ClosestPoint(transform.position);
-
-           
-                    player.getDmg(dmg, dmgType, percentageDistribution, point, transform);
-
-
+                player.getDmg(dmg, dmgType, percentageDistribution, point, transform);
             }
         }
         if (other.tag == "Shield")
         {
             if (!player.dead)
             {
-
-
                 Vector3 point;
-
                 point = other.gameObject.GetComponent<Collider2D>().bounds.ClosestPoint(transform.position);
-
-
                 other.GetComponent<Shield>().getDmg(dmg, dmgType, percentageDistribution, point, transform);
-
             }
         }
     }
-    // Update is called once per frame
-
-
-
-
-
 }
