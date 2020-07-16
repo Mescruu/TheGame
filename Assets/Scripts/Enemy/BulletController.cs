5940fee2ea891e5ca67843b51c6183dd6bb52009
Mescruu <mescruu@gmail.com>
2020-07-16 16:48:26 +0200

    Typical bullet movement, dmg, destroy

--- Assets/Scripts/Enemy/BulletController.cs
+++ Assets/Scripts/Enemy/BulletController.cs
@@ -4,44 +4,47 @@ using UnityEngine;
 
 public class BulletController : MonoBehaviour {
 
+    //Typ obrażeń
     [Tooltip("Type of dmg")]
     public DmgType[] dmgType;
 
+    //Ilość obrażeń
     [Tooltip("Count of dmg")]
     public float dmg;
-    [Tooltip("Percentage Distribution of all dmg's types for example: 0.1 = 10% ")]
-    public float[] percentageDistribution;  //rozklad obrazen
 
+    //rozklad obrazen
+    [Tooltip("Percentage Distribution of all dmg's types for example: 0.1 = 10% ")]
+    public float[] percentageDistribution; 
+
+    //W co ma trafić
     public Transform target;
 
+    //Prędkość pocisku
     [Tooltip("Speed")]
     public float speed = 10;
 
+    //Obiekt gracza
     private Player_Controller player;
-    private Destroy_obj destr;
 
+    //Komponent destrukcji
+    private Destroy_obj destr;
 
     void Start()
     {
+        //dołączenie obiektu gracza
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player_Controller>();
 
-        // Cache our start position, which is really the only thing we need
-        // (in addition to our current position, and the target).
+        //dołączenie komponentu destrukcji
         destr = gameObject.GetComponent<Destroy_obj>();
 
+        //Zapisanie pozycji gdzie należy wysłać "pocisk"
         target.transform.position = player.transform.position;
-
-
     }
-    // Use this for initialization
- 
-	
-	// Update is called once per frame
+
 	void Update ()
     {
-
+        //Poruszanie się w stronę początkowego punktu
         transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
-
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -50,32 +53,24 @@ public class BulletController : MonoBehaviour {
         {
             if (!player.dead)
             {
-
-
-                Vector3 point;
-
+                Vector3 point; //nowy punkt określający miejsce trafienia
                 point = other.gameObject.GetComponent<Collider2D>().bounds.ClosestPoint(transform.position);
+                
+                player.getDmg(dmg, dmgType, percentageDistribution, point, transform);
 
-              
-                    player.getDmg(dmg, dmgType, percentageDistribution, point, transform);
-
-                if (destr != null)
+                if (destr != null) //jeżeli komponent został dołączony zniszcz obiekt
                 {
                     destr.TimeToDestroy = 0;
                 }
 
             }
         }
-        if (other.tag == "Shield")
+        if (other.tag == "Shield") // w przypadku jeżeli gracz używa tarczy obrażenia otrzymuje ona
         {
             if (!player.dead)
             {
-
-
                 Vector3 point;
-
                 point = other.gameObject.GetComponent<Collider2D>().bounds.ClosestPoint(transform.position);
-
 
                 other.GetComponent<Shield>().getDmg(dmg, dmgType, percentageDistribution, point, transform);
 
@@ -83,13 +78,11 @@ public class BulletController : MonoBehaviour {
                 {
                     destr.TimeToDestroy = 0;
                 }
-
             }
         }
-        if (other.tag == "Ground")
+        //jeżeli trafi w ziemie, bądź ściane
+        if (other.tag == "Ground" || other.tag == "Wall") 
         {
-
-
             if (destr != null)
             {
                 destr.TimeToDestroy = 0;
@@ -98,22 +91,6 @@ public class BulletController : MonoBehaviour {
             {
                 Destroy(gameObject);
             }
-
-        }
-        if (other.tag == "Wall")
-        {
-
-
-            if (destr != null)
-            {
-                destr.TimeToDestroy = 0;
-            }
-            else
-            {
-                Destroy(gameObject);
-            }
-
         }
     }
-
 }
