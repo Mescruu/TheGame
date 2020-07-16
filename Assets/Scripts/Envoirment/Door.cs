047aaa41d22fad5bbde3a425e1f1d4e238bb44f3
Mescruu <mescruu@gmail.com>
2020-07-16 21:07:37 +0200

    Passage another level

--- Assets/Scripts/Envoirment/Door.cs
+++ Assets/Scripts/Envoirment/Door.cs
@@ -5,32 +5,39 @@ using UnityEngine.UI;
 public class Door : MonoBehaviour {
 
 
-	public int DoorID;
-	public int LevelToLoad;
-	private Game_Master gm;
-	public GameObject desk;
+	public int DoorID; //Id drzwi
+	public int LevelToLoad; //który level ma zostać załadowany
+
+    //dane podpowiedzi
+    public GameObject desk;
     public Text text;
 	public bool loadLev;
-	private Player_Controller player;
-    private KeyMenager keyMenager;
+	
+    //Czas po którym podpowiedź zniknie
     private float ExitTimeCD=1f;
     private float ExitTime;
     private bool Exit;
-	void Start()
+
+    private Player_Controller player;
+    private KeyMenager keyMenager;
+    private Game_Master gm;
+    void Start()
 	{
-		gm = GameObject.FindGameObjectWithTag ("GameMaster").GetComponent<Game_Master> ();
+        //Początkowe wartości
         desk.SetActive(false);
-        keyMenager = GameObject.Find("KeyMenager").GetComponent<KeyMenager>();
-        player = GameObject.Find("Player").GetComponent<Player_Controller>();
         ExitTime = ExitTimeCD;
         Exit = false;
+
+        //Dołaczenie komponentów
+        gm = GameObject.FindGameObjectWithTag("GameMaster").GetComponent<Game_Master>();
+        keyMenager = GameObject.Find("KeyMenager").GetComponent<KeyMenager>();
+        player = GameObject.Find("Player").GetComponent<Player_Controller>();
     }
     void Update()
     {
-        if(Exit)
+        if(Exit)//Zniknięcie podpowiedzi po danym czasie
         {
             desk.GetComponent<Animator>().SetBool("disappear", true);
-
             ExitTime -= Time.deltaTime;
             if (ExitTime < 0)
             {
@@ -46,25 +53,20 @@ public class Door : MonoBehaviour {
     }
     void OnTriggerEnter2D(Collider2D col)
 	{
-
-        if (col.gameObject.tag == "Player")
+        if (col.gameObject.tag == "Player") //Atywowwanie podpowiedzi
         {
-
             desk.SetActive(true);
             text.text = "[" +keyMenager.keys["Action"] + "] To enter";
             Exit = false;
-			}
-
-
+		}
 	}
 
 	void OnTriggerStay2D(Collider2D col)
 	{
-
         if (col.gameObject.tag=="Player" && player.curHP>0)
-			    {
-				    if(Input.GetKeyDown(keyMenager.keys["Action"]) || loadLev)
-				    {
+		{
+			if(Input.GetKeyDown(keyMenager.keys["Action"]) || loadLev) //PRzejście do nastepnego poziomu
+			{
                 if(player.curHP>0)
                 {
                     gm.DoorID = DoorID;
@@ -77,17 +79,12 @@ public class Door : MonoBehaviour {
         }
 	}
 
-	void OnTriggerExit2D(Collider2D col)
+	void OnTriggerExit2D(Collider2D col) //Gracz opuścił kolizję 
 	{
-
         if (col.gameObject.tag == "Player")
         {
             ExitTime = ExitTimeCD;
             Exit = true;
 		}
-
-
 	}
-
-
 }
