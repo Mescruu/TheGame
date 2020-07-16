a277fae9bf66bf632c1c790af83f6ac7d4edad79
Mescruu <mescruu@gmail.com>
2020-07-16 20:59:59 +0200

    Echo after object

--- Assets/Scripts/EchoEffect.cs
+++ Assets/Scripts/EchoEffect.cs
@@ -5,25 +5,22 @@ using UnityEngine;
 public class EchoEffect : MonoBehaviour {
 
     private float DelayEcho;
-    public float DelayEchoCD=0.01f;
+    public float DelayEchoCD=0.01f; //Opóźnienie za obiektem właściwym
 
     public GameObject echo;
     public bool MakeAnEcho;
     private Rigidbody2D rgb2d;
     public float echoBeginning=150f;
+    public float EchoTime = 1f; // Jak długo echo trwa
 
-    public float EchoTime = 1f; // How long echo exist
-
-    // Skrypt służący do tworzenia echa/klonu obiektu
+    // Skrypt służący do tworzenia echa/klonu obiektu za nim, na zasadzie smugi
     void Start () {
        DelayEchoCD = DelayEcho;
        MakeAnEcho =false;
        rgb2d = gameObject.GetComponent<Rigidbody2D>();
 	}
 	
-	// Update is called once per frame
 	void Update () {
-
         if (MakeAnEcho)
         {
             if (Mathf.Abs(rgb2d.velocity.x) > echoBeginning || Mathf.Abs(rgb2d.velocity.y) > echoBeginning)
