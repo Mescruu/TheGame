e65420ebdbb409eb17f6cba21508906959bfb2cb
Mescruu <mescruu@gmail.com>
2020-07-17 13:57:12 +0200

    Loading scenes caller

--- Assets/Scripts/Game/LoadTargetScript.cs
+++ Assets/Scripts/Game/LoadTargetScript.cs
@@ -5,17 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class LoadTargetScript : MonoBehaviour {
 
-
     public void LoadScreenNum(int num, int fromScene)
     {
-        if (num < 0 || num >= SceneManager.sceneCountInBuildSettings)
+        if (num < 0 || num >= SceneManager.sceneCountInBuildSettings) //błąd
         {
             Debug.LogWarning("Cant load scene num" + num + ", Scene manager only has " + SceneManager.sceneCountInBuildSettings + " scenes in BuildSettings!");
             return;
         }
-            LoadingScreenManager.LoadScene(num, fromScene);
-
+            LoadingScreenManager.LoadScene(num, fromScene); //wywołanie skryptu umożliwiającego ładowanie sceny
     }
-
 }
 
