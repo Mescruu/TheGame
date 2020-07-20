16e76dd1735e8cb591fee7a3e6ab48e9173c1061
Mescruu <mescruu@gmail.com>
2020-07-20 17:39:41 +0200

    Throwing object controller - knife fireball etc.

--- Assets/Scripts/Player/Player Attack Shit/knifeController.cs
+++ Assets/Scripts/Player/Player Attack Shit/knifeController.cs
@@ -6,40 +6,43 @@ public class knifeController : MonoBehaviour {
     public DmgType[] DmgType;  //typy obrazen
     public float[] percentageDistribution;  //rozklad obrazen
 
-    public bool ShouldAppearObject;
+    public bool ShouldAppearObject; //tworzenie dodatkowego obiektu
 	public GameObject ObjectToAppear;
+
+    //prędkość obiektu
 	public float speed;
-
-	private Rigidbody2D rb2d;
-
 	public float dmg = 20;
 
+    //trzęsienie kamery
 	public bool shouldShakeCamera;
 	public float ShakePow;
 	public float ShakeDur;
-	private Player_Controller player;
-	public Camera_Following cam;
-    private Game_Master gm;
-    private bool HasAMeeleDMG;
-    public bool Knife;
-    public bool ShouldAppearObjectOnEnemy;
+
+    private bool HasAMeeleDMG; //czy ma obrazenia zwykłe
+    public bool Knife; //czy to nóż
+    public bool ShouldAppearObjectOnEnemy; //czy pojawić jakiś dodatkowy obiekt na przeciwniku
+
     public GameObject ObjectToAppearOnEnemy;
     public Transform PointToAppearObj;
-    private AudioSource audioSource;
-    // Use this for initialization
+
+    //komponenty
+    private AudioSource audioSource; //odtwarzacz dźwięku
+    private Rigidbody2D rb2d; //fizyka obiektu
+    private Player_Controller player;
+    public Camera_Following cam;
+    private Game_Master gm;
+
     void Start () {
 		player = FindObjectOfType<Player_Controller> ();
-        //odwrócenie punktu strzalu
         cam = GameObject.Find("Main Camera").GetComponent<Camera_Following>();
         gm = GameObject.FindGameObjectWithTag("GameMaster").GetComponent<Game_Master>();
         HasAMeeleDMG = false;
         rb2d = gameObject.GetComponent<Rigidbody2D>();
 
        
-        if(Knife)
+        if(Knife) //jeżeli to jest nóż z ekwipunku
         {
-            dmg = gm.Secon_Weapon_Slot.GetComponentInChildren<ElementEq>().attack;
-
+            dmg = gm.Secon_Weapon_Slot.GetComponentInChildren<ElementEq>().attack; //pobranie wartości obrażeń z obiektu w ekwipunku
             dmg = gm.PlayerStatsAttack * dmg * 0.1f + dmg;   //mnożnik ataku
             HasAMeeleDMG = true;
         }
@@ -54,49 +57,38 @@ public class knifeController : MonoBehaviour {
             }
         }
 
-
-
-
-    
-
-        if (player.facingRight)
-			{
-				transform.localScale = new Vector3(1, 1, 1);
-			}
-			else
-			{
-				transform.localScale = new Vector3(-1, 1, 1);
-                speed = -speed;
-
-            }
-      //  Random.Range(dmg, dmg + 10f);
-
-
+        if (player.facingRight) //ustawienie skali obiektu
+		{
+			transform.localScale = new Vector3(1, 1, 1);
+		}
+		else
+		{
+			transform.localScale = new Vector3(-1, 1, 1);
+            speed = -speed;
+        }
     }
 
-    // Update is called once per frame
-
     void Update () {
-      
+      //poruszanie się obiektu
 		GetComponent<Rigidbody2D> ().velocity = new Vector2 (speed, GetComponent<Rigidbody2D> ().velocity.y);
     }
 
 	void OnTriggerEnter2D(Collider2D other)
 	{
-				if (other.tag == "Enemy") 
-				{
-                Enemy enemy = other.GetComponent<Enemy>();
-                    if(!enemy.Dead)
-                    {
-                        if (shouldShakeCamera)
-                        {
-                        cam.ShakeCamera(ShakePow, ShakeDur);
-                        }
+		if (other.tag == "Enemy")  //zadanie obrażeń przeciwnikowi / niszczenie obiektu
+		{
+            Enemy enemy = other.GetComponent<Enemy>();
+             if(!enemy.Dead)
+             {
+                 if (shouldShakeCamera)
+                 {
+                    cam.ShakeCamera(ShakePow, ShakeDur);
+                 }
                 DestroyObj();
                 if (ShouldAppearObject)
-                        {
+                {
                         Instantiate(ObjectToAppear, transform.position, transform.rotation);
-                        }
+                }
                 if (ShouldAppearObjectOnEnemy)
                 {
                     Instantiate(ObjectToAppearOnEnemy, PointToAppearObj.position, transform.rotation);
@@ -115,13 +107,10 @@ public class knifeController : MonoBehaviour {
                     {
                         Instantiate(enemy.bloodBack, point, enemy.transform.rotation);
                         enemy.CheckYourBack();
-
                     }
                 }
-             
-
             }
-                }
+         }
 
 
 		if (other.tag == "FlyerEnemy") 
@@ -129,40 +118,33 @@ public class knifeController : MonoBehaviour {
 			if(shouldShakeCamera)
 			{
 				cam.ShakeCamera(ShakePow,ShakeDur);
-				
 			}
             DestroyObj();
             if (ShouldAppearObject)
 			{	
-
 				Instantiate (ObjectToAppear, transform.position, transform.rotation);
-				
 			}
 		}
 
-
+        //niszczenie obiektu
 		if (other.tag == "Walls") 
 		{
 			if(shouldShakeCamera)
 			{
 				cam.ShakeCamera(ShakePow,ShakeDur);
-				
 			}
             DestroyObj();
             if (ShouldAppearObject)
 			{
 				Instantiate (ObjectToAppear, transform.position, transform.rotation);
-				
 			}
 		}
-
 
 		if (other.tag == "Ground") 
 		{
 			if(shouldShakeCamera)
 			{
 				cam.ShakeCamera(ShakePow,ShakeDur);
-				
 			}
 			if(ShouldAppearObject)
 			{
@@ -173,11 +155,10 @@ public class knifeController : MonoBehaviour {
 		}
 		
 	}
-    void DestroyObj()
+    void DestroyObj() //niszczenie obiektu
     {
         if(gameObject.GetComponentInChildren<ParticleSystem>()!=null)
         {
-
             float timetoDestroy = GetComponentInChildren<ParticleSystem>().startLifetime;
             gameObject.GetComponentInChildren<ParticleSystem>().enableEmission=false;
             Destroy(gameObject, timetoDestroy);
