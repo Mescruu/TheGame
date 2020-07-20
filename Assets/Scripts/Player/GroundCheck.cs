7bef7bb6feab85965d21972d3fc7ed7fdda90245
Mescruu <mescruu@gmail.com>
2020-07-20 16:57:53 +0200

    Script checking that Player is grounded

--- Assets/Scripts/Player/GroundCheck.cs
+++ Assets/Scripts/Player/GroundCheck.cs
@@ -5,44 +5,40 @@ public class GroundCheck : MonoBehaviour {
 
 	// Use this for initialization
 
-	private Player_Controller player;
-    public AudioSource groundSound;
-    public GameObject Dust;
-    public Transform DustPos;
-
+	private Player_Controller player; //obiekt gracza
+    public AudioSource groundSound; //dźwięk poruszania się
+    public GameObject Dust; //kurz
+    public Transform DustPos; //pozycja nóg
 
     void Start()
 	{
-		player = gameObject.GetComponentInParent<Player_Controller> ();
+		player = gameObject.GetComponentInParent<Player_Controller> (); //czym jest gracz
 	}
 
 	void OnTriggerEnter2D(Collider2D col)
 	{	
-		if (col.gameObject.tag == "Ground") 
-				{
-            groundSound.Play();
-            player.grounded = true;
-            Instantiate(Dust, DustPos.position, transform.rotation);
-                }
+		if (col.gameObject.tag == "Ground")  //jeżeli jest to ziemia utworz cząstki kurzu i zagraj dźwięk
+		{
+			groundSound.Play();
+			player.grounded = true;
+			Instantiate(Dust, DustPos.position, transform.rotation);
+         }
 	}
 
-	void OnTriggerStay2D(Collider2D col)
+	void OnTriggerStay2D(Collider2D col) //gracz jest na ziemi (nie w powietrzu)
 	{
 		if (col.gameObject.tag == "Ground") 
 		{
 			player.grounded = true;
 		}
-
-
 	}
 
-	void OnTriggerExit2D(Collider2D col)
+	void OnTriggerExit2D(Collider2D col) //gracz wyskoczył - nie jest juz na ziemi
 	{
         if (col.gameObject.tag == "Ground")
         {
             player.grounded = false;
             groundSound.Play();
-
         }
     }
 }   
