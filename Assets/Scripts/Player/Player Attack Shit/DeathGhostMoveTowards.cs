6eb02bc4fc40bb4d6f2d20fd32034e2c41b992f1
Mescruu <mescruu@gmail.com>
2020-07-20 17:32:33 +0200

    Ghost from death mode controller

--- Assets/Scripts/Player/Player Attack Shit/DeathGhostMoveTowards.cs
+++ Assets/Scripts/Player/Player Attack Shit/DeathGhostMoveTowards.cs
@@ -5,87 +5,69 @@ public class DeathGhostMoveTowards : MonoBehaviour {
 
     private GameObject playerGameObj;
 
-    private Player_Controller player;
-	private PlayerAttack playerAtck;
-	public Transform playerPoint;
+	//wartości
 	public float moveSpeed;
 	public float normalMoveSpeed;
 	public float highSpeed;
 	public bool playerInRange;
+
+	//komponenty
+	private Player_Controller player;
+	private PlayerAttack playerAtck;
+	public Transform playerPoint;
 	public LayerMask playerLayer;
-	public float PlayerRange;
+	public float PlayerRange; //pole w którym jest gracz blisko
 	private Animator anim;
     private Rigidbody2D rgb2;
 
-	// Use this for initialization
 	void Start () {
-
-
+		//komponenty
         playerGameObj = GameObject.FindGameObjectWithTag("Player");
         player = playerGameObj.GetComponent<Player_Controller>();
         playerAtck = playerGameObj.GetComponent<PlayerAttack>();
         playerPoint = playerAtck.DeathMaskPoint;
-
         rgb2 = gameObject.GetComponent<Rigidbody2D>();
-
 		anim = gameObject.GetComponent<Animator> ();
-
-
 	}
 	
-	// Update is called once per frame
 	void Update ()
-		
 	{
-
-		transform.position = Vector3.MoveTowards (transform.position, playerPoint.transform.position, moveSpeed);
-		
+		//poruszanie się duszka
+		transform.position = Vector3.MoveTowards (transform.position, playerPoint.transform.position, moveSpeed); 
 		playerInRange = Physics2D.OverlapCircle (transform.position, PlayerRange, playerLayer);
-
-			
-			if(transform.position.x<playerPoint.position.x)
-			{
+		
+		//odwracanie obiektu
+		if(transform.position.x<playerPoint.position.x)
 				transform.localScale = new Vector3(1, 1, 1);
-			}
+
         if (transform.position.x > playerPoint.position.x)
-             {
 				transform.localScale = new Vector3(-1, 1, 1);	
-              }
+
         if (transform.position.x == playerPoint.position.x)
         {
             if(player.facingRight)
             {
                 transform.localScale = new Vector3(1, 1, 1);
-
             }
             else
             {
                 transform.localScale = new Vector3(-1, 1, 1);
-
             }
         }
 
         if (playerInRange) {
-						moveSpeed = normalMoveSpeed;
-				} else {
+			moveSpeed = normalMoveSpeed;
+		} else {
 			moveSpeed = highSpeed;
-
-				}
+		}
 	
-		if (playerAtck.DeathMode == false) 
+		if (playerAtck.DeathMode == false)  //zniszczenie obiektu po zakończeniu działania czaru
 		{
-
-
 			Destroy (gameObject);
-
 		}
 		if (playerAtck.DeathModeTime <= 0.5f) 
 		{
-
-
 			anim.SetBool ("Dead", true);
-
-
 		}
-}
+	}
 }
